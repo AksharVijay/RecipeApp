348c74b83563d033dc80184f0ba34825d3c2e302
Akshar Vijay <aksharvij@gmail.com>
2023-05-31 05:45:58 +1200

    components

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Trending from '../../components/Trending/Trending';
+import Vegetarian from '../../components/Vegetarian/Vegetarian';
+
+const Home = () => {
+  return (
+    <>
+        <Trending/>
+        <Vegetarian/>
+    </>
+  )
+}
+
+export default Home;

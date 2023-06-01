0b9c5c3c04565b71ddace6a82156985c9a8352c1
Akshar Vijay <aksharvij@gmail.com>
2023-06-02 02:43:21 +1200

    fetch data

--- src/components/Trending/Trending.jsx
+++ src/components/Trending/Trending.jsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
+
 const Trending = () => {
+
+  useEffect(() => {
+    getTrending();
+  },[])
+
+  const getTrending = async() => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=f9a3b5aa7e284b55ba91945b369a8628&number=9`
+    );
+    const data = await api.json();
+    console.log(data);
+  }
   return (
-    <div>Trending</div>
+    <div>
+      Trending
+    </div>
   )
 }
 
